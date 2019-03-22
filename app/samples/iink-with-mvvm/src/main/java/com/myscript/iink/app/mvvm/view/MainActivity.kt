8f59dfc4c3dc9b37f485acd05a35194eb2b92ed3
Jing KE <jing.ke@myscript.com>
2019-03-22 21:31:30 +0100

    [sample][2][iink-with-mvvm] refactor `activities` → `view`.

--- app/samples/iink-with-mvvm/src/main/java/com/myscript/iink/app/mvvm/view/MainActivity.kt
+++ app/samples/iink-with-mvvm/src/main/java/com/myscript/iink/app/mvvm/view/MainActivity.kt
@@ -2,7 +2,7 @@
  * Copyright (c) MyScript. All rights reserved.
  */
 
-package com.myscript.iink.app.mvvm.activities
+package com.myscript.iink.app.mvvm.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
